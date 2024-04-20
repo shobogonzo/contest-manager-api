1c4626e5d0d69465fdf84608de5e15509744504d
Patrick Palmer <ppalmer@pm.me>
2024-04-20 10:06:42 -0500

    Add e2e test file and update GitHub actions

---
+++ tests/test-cases/e2e/user-management.tests.ts
@@ -0,0 +1,7 @@
+describe('TODO', () => {
+  // TODO user can update own profile
+  // TODO user cannot update another's profile
+  it('1 should equal 1', () => {
+    expect(1).toEqual(1)
+  })
+});
